62568e97030d2c07bef98a983b967b4a9783baea
ZhenfengSu <ZhenfengSu@outlook.com>
2026-01-11 14:32:35 +0000

    add some change

---
+++ baseline/Kaleidoscope/Kalei_SMACv2/src/zerg_huairou2_.bash
@@ -0,0 +1,3 @@
+export CUDA_VISIBLE_DEVICES=0
+python src/main.py --config=Kalei_qmix_rnn_1R3 --env-config=sc2_gen_zerg --exp-config=zerg_5v5_10M_s0
+python src/main.py --config=Kalei_qmix_rnn_1R3 --env-config=sc2_gen_zerg --exp-config=zerg_5v5_10M_s0
