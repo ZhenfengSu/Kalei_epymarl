28c93bb2e86df9987644db17cf5af58bd456e8ee
ZhenfengSu <ZhenfengSu@outlook.com>
2026-01-11 15:59:32 +0000

    add k24 for mpe

---
+++ Semi_Kalei/epymarl_kaleidoscope/world copy.bash
@@ -0,0 +1,16 @@
+export CUDA_VISIBLE_DEVICES=2
+python src/main.py --config=K24_nq --env-config=gymma with env_args.time_limit=25 env_args.key="pz-mpe-simple-world-comm-v3"
+python src/main.py --config=qmix --env-config=gymma with env_args.time_limit=25 env_args.key="pz-mpe-simple-world-comm-v3"
+python src/main.py --config=qmix --env-config=gymma with env_args.time_limit=25 env_args.key="pz-mpe-simple-world-comm-v3"
+
+python src/main.py --config=qmix_ns --env-config=gymma with env_args.time_limit=25 env_args.key="pz-mpe-simple-world-comm-v3"
+python src/main.py --config=qmix_ns --env-config=gymma with env_args.time_limit=25 env_args.key="pz-mpe-simple-world-comm-v3"
+python src/main.py --config=qmix_ns --env-config=gymma with env_args.time_limit=25 env_args.key="pz-mpe-simple-world-comm-v3"
+
+python src/main.py --config=Kalei_qmix_rnn_1R3 --env-config=gymma with env_args.time_limit=25 env_args.key="pz-mpe-simple-world-comm-v3"
+python src/main.py --config=Kalei_qmix_rnn_1R3 --env-config=gymma with env_args.time_limit=25 env_args.key="pz-mpe-simple-world-comm-v3"
+python src/main.py --config=Kalei_qmix_rnn_1R3 --env-config=gymma with env_args.time_limit=25 env_args.key="pz-mpe-simple-world-comm-v3"
+python src/main.py --config=Kalei_qmix_rnn_1R3 --env-config=gymma with env_args.time_limit=25 env_args.key="pz-mpe-simple-world-comm-v3"
+python src/main.py --config=Kalei_qmix_rnn_1R3 --env-config=gymma with env_args.time_limit=25 env_args.key="pz-mpe-simple-world-comm-v3"
+
+
