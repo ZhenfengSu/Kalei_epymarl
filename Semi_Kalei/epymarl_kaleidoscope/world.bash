d681322d1414e5e91c20acec164b58c306d1c40d
ZhenfengSu <ZhenfengSu@outlook.com>
2026-01-14 03:11:49 +0000

    add run bash

--- Semi_Kalei/epymarl_kaleidoscope/world.bash
+++ Semi_Kalei/epymarl_kaleidoscope/world.bash
@@ -1,4 +1,5 @@
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=3
 # python src/main.py --config=K24_nq --env-config=gymma with env_args.time_limit=25 env_args.key="pz-mpe-simple-world-comm-v3" seed=0
+python src/main.py --config=K24_nq --env-config=gymma with env_args.time_limit=25 env_args.key="pz-mpe-simple-world-comm-v3" seed=0
 python src/main.py --config=K24_nq --env-config=gymma with env_args.time_limit=25 env_args.key="pz-mpe-simple-world-comm-v3" seed=1
 
