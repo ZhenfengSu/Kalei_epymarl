2b53c1338c88a2c7409656e3ed8c2fd8004948c5
ZhenfengSu <ZhenfengSu@outlook.com>
2026-01-15 09:05:30 +0000

    update bash

---
+++ Semi_Kalei/epymarl_kaleidoscope/listener_baseline.bash
@@ -0,0 +1,17 @@
+export CUDA_VISIBLE_DEVICES=0
+# python src/main.py --config=qmix --env-config=gymma with env_args.time_limit=25 env_args.key="pz-mpe-simple-speaker-listener-v4"
+# python src/main.py --config=qmix --env-config=gymma with env_args.time_limit=25 env_args.key="pz-mpe-simple-speaker-listener-v4"
+# python src/main.py --config=qmix --env-config=gymma with env_args.time_limit=25 env_args.key="pz-mpe-simple-speaker-listener-v4"
+
+# python src/main.py --config=qmix_ns --env-config=gymma with env_args.time_limit=25 env_args.key="pz-mpe-simple-speaker-listener-v4"
+# python src/main.py --config=qmix_ns --env-config=gymma with env_args.time_limit=25 env_args.key="pz-mpe-simple-speaker-listener-v4"
+# python src/main.py --config=qmix_ns --env-config=gymma with env_args.time_limit=25 env_args.key="pz-mpe-simple-speaker-listener-v4"
+
+# python src/main.py --config=Kalei_qmix_rnn_1R3 --env-config=gymma with env_args.time_limit=25 env_args.key="pz-mpe-simple-speaker-listener-v4"
+# python src/main.py --config=Kalei_qmix_rnn_1R3 --env-config=gymma with env_args.time_limit=25 env_args.key="pz-mpe-simple-speaker-listener-v4"
+# python src/main.py --config=Kalei_qmix_rnn_1R3 --env-config=gymma with env_args.time_limit=25 env_args.key="pz-mpe-simple-speaker-listener-v4"
+# python src/main.py --config=Kalei_qmix_rnn_1R3 --env-config=gymma with env_args.time_limit=25 env_args.key="pz-mpe-simple-speaker-listener-v4"
+# python src/main.py --config=Kalei_qmix_rnn_1R3 --env-config=gymma with env_args.time_limit=25 env_args.key="pz-mpe-simple-speaker-listener-v4"
+
+
+python src/main.py --config=K24_nq --env-config=gymma with env_args.time_limit=25 env_args.key="pz-mpe-simple-speaker-listener-v4"
